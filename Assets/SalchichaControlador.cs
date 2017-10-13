4ed69cbad61ee695ae270f5bca4acf3d48f21212
Anthony Vargas <tony_jason@hotmail.com>
2017-10-13 12:11:48 -0500

    click event en las salchicchas se necesita un 2dCollider para que se emita

---
+++ Assets/SalchichaControlador.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalchichaControlador : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("iniciando salchichas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		Debug.Log ("mouse down");
+	}
+
+	void OnMouseOver(){
+		Debug.Log ("mouse over");
+	}
+}
