097850dd9c9d8d8616095f618de733dcf0a482a3
Anthony Vargas <tony_jason@hotmail.com>
2017-10-14 15:24:49 -0500

    cocinado de salchichas

---
+++ Assets/Cocinado.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cocinado : MonoBehaviour {
+
+	public float tiempoCocinado;
+	private float tiempoActualCocinado=0;
+	private string estado = "1";
+	public Sprite salchicha2;
+	public Sprite salchicha3;
+	public Sprite salchicha4;
+	public Sprite salchicha5;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (GetComponent<SpriteRenderer> ().sprite);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tiempoActualCocinado += Time.deltaTime; 
+		if (tiempoActualCocinado >= tiempoCocinado && estado == "1") {
+			GetComponent<SpriteRenderer>().sprite = salchicha2 ;
+			estado = "2";
+		}
+		if (tiempoActualCocinado >= tiempoCocinado*2 && estado == "2") {
+			GetComponent<SpriteRenderer>().sprite = salchicha3 ;
+			estado = "3";
+		}
+		if (tiempoActualCocinado >= tiempoCocinado*3 && estado == "3") {
+			GetComponent<SpriteRenderer>().sprite = salchicha4 ;
+			estado = "4";
+		}
+		if (tiempoActualCocinado >= tiempoCocinado*4 && estado == "4") {
+			GetComponent<SpriteRenderer>().sprite = salchicha5 ;
+			estado = "5";
+		}
+
+		//Debug.Log (tiempoCocinado);
+	}
+}
--- Assets/SalchichaControlador.cs
+++ Assets/SalchichaControlador.cs
@@ -4,9 +4,46 @@ using UnityEngine;
 
 public class SalchichaControlador : MonoBehaviour {
 
+	public Transform salchicha;
+	public PosicionParrilla posParrilla1;
+	public PosicionParrilla posParrilla2;
+	public PosicionParrilla posParrilla3;
+	public PosicionParrilla posParrilla4;
+
+	public class PosicionParrilla{
+	
+		public string estado;
+		public Vector3 v3Pos;
+
+		public PosicionParrilla(Vector3 pos){
+			estado = "vacio";
+			v3Pos = pos;
+		}
+
+	}
+
+	PosicionParrilla getSigPosLibre(){
+		if (posParrilla1.estado == "vacio") {
+			return posParrilla1;
+			}
+		else if(posParrilla2.estado == "vacio") {
+			return posParrilla2;
+		}
+		else if(posParrilla3.estado == "vacio") {
+			return posParrilla3;
+		}
+		else if(posParrilla4.estado == "vacio") {
+			return posParrilla4;
+		}
+		return null;
+	}
+
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("iniciando salchichas");
+		posParrilla1 = new PosicionParrilla(new Vector3 (1.56f, -0.35f, 0));
+		posParrilla2 = new PosicionParrilla(new Vector3 (1.91f, -0.35f, 0));
+		posParrilla3 = new PosicionParrilla(new Vector3 (2.26f, -0.35f, 0));
+		posParrilla4 = new PosicionParrilla(new Vector3 (2.64f, -0.35f, 0));
 	}
 	
 	// Update is called once per frame
@@ -15,10 +52,12 @@ public class SalchichaControlador : MonoBehaviour {
 	}
 
 	void OnMouseDown(){
-		Debug.Log ("mouse down");
+		PosicionParrilla posLibre = getSigPosLibre();
+		if (posLibre != null) {
+			Instantiate (salchicha, posLibre.v3Pos, Quaternion.identity);
+			posLibre.estado = "ocupado";
+		}
 	}
 
-	void OnMouseOver(){
-		Debug.Log ("mouse over");
-	}
+
 }
