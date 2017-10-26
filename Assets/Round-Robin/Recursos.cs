cdadbf35a8f54e01e75e6adf2bc0489bee62fe9c
Anthony Vargas <tony_jason@hotmail.com>
2017-10-25 22:02:33 -0500

    texto quantum

---
+++ Assets/Round-Robin/Recursos.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recursos : MonoBehaviour {
+
+	public static Dictionary<string,Recurso> lista;
+
+	public class Recurso{
+
+		public string nombre;
+		public string estado;
+
+		public Recurso(string nombre){
+			this.nombre = nombre;
+			estado = "libre";
+		}
+
+		public string getEstado(){
+			return estado;
+		}
+
+		public void setEstado(string nuevoEstado){
+			estado = nuevoEstado;
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		lista = new Dictionary<string,Recurso> ();
+		Recurso salsaTomate = new Recurso ("salsaTomate");
+		Recurso mostaza = new Recurso ("mostaza");
+		lista.Add (salsaTomate.nombre, salsaTomate);
+		lista.Add (mostaza.nombre, mostaza);
+	}
+
+	public static string getEstadoRecurso(string nombre){
+		return lista [nombre].getEstado ();
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Round-Robin/planificador.cs
+++ Assets/Round-Robin/planificador.cs
@@ -1,35 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class planificador : MonoBehaviour {
 
 	seleccionTipoPerro seleccionPerro;
 	public PersonajeController controladorPersonajes;
+	public Text quantumTX;
 
 	//orden analogo a proceso
 	public GameObject Orden;
 	//colas de los procesos
-	Queue listos;
-	Queue bloqueados;
-	Queue suspendidos;
+	Queue<Proceso> listos;
+	Queue<Proceso> bloqueados;
+	Queue<Proceso> suspendidos;
 
+	Proceso procesoEnEjecucion;
 
+	public class Proceso
+	{
+		float Quantum = 20; //segundos;
+		float tiempoRestante;
+
+		public Proceso(){
+			tiempoRestante = Quantum;
+		}
+
+		public void ejecutar(float tiempo){
+			if (tiempoRestante > 0)
+				tiempoRestante -= tiempo;
+			else
+				tiempoRestante = 0;
+		}
+
+		public float getTiempoRestante(){
+			return tiempoRestante;
+		}
+
+	}
+
+	//crear proceso
 	public void crearOrden(){
 		Debug.Log ("crear orden");
 		Debug.Log ("tipo perro: " + seleccionPerro.getTipoPerro ());
 		controladorPersonajes.agregarPersonaje();
+		Debug.Log (Recursos.getEstadoRecurso ("salsaTomate"));
+
+		Proceso nuevoProceso = new Proceso ();
+		listos.Enqueue (nuevoProceso);
 
 	}
 
 	// Use this for initialization
 	void Start () {
 		seleccionPerro = GetComponent<seleccionTipoPerro> ();
-
+		listos = new Queue<Proceso> ();
+		bloqueados = new Queue<Proceso> ();
+		suspendidos = new Queue<Proceso> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (procesoEnEjecucion == null) {
+			if (listos.Count > 0 ) {
+				procesoEnEjecucion = listos.Dequeue ();
+			}
+		} 
+		else {
+			procesoEnEjecucion.ejecutar (Time.deltaTime);	
+			quantumTX.text = System.Convert.ToString (procesoEnEjecucion.getTiempoRestante ());
+			Debug.Log (procesoEnEjecucion.getTiempoRestante ());
+		}
+
 	}
 }
