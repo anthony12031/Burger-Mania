c1f7a4917491539dfe058979d30ab6791d235f6f
Anthony Vargas <tony_jason@hotmail.com>
2017-10-27 17:33:54 -0500

    cambio de estado de salchichas de acuerdo a sonido

--- Assets/scrpts/Cocinado.cs
+++ Assets/scrpts/Cocinado.cs
@@ -7,6 +7,7 @@ public class Cocinado : MonoBehaviour {
 	public float tiempoCocinado;	
 	public bool estaEnParrilla = true;
 	private Animator animator;
+	public AudioClip sonidoListo;
 
 	// Use this for initialization
 	void Start () {
@@ -21,5 +22,19 @@ public class Cocinado : MonoBehaviour {
 			animator.SetInteger ("tiempo", (int)tiempoCocinado);
 		}
 
+		//Debug.Log (GetComponent<AudioSource> ().isPlaying);
+		if (!GetComponent<AudioSource> ().isPlaying) {
+			animator.SetBool ("cocinado", true);
+		}
+
 	}
+
+	bool eventoCocidoLlamado = false;
+
+	public void eventoCocido(){
+		if(!eventoCocidoLlamado)
+			GetComponent<AudioSource> ().PlayOneShot (sonidoListo);
+		eventoCocidoLlamado = true;
+	}
+
 }
