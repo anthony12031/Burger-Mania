e9e664768bbb2a936322696f0bfc7212f0c9136f
Sebastian Bonilla <js.bonilla@hotmail.com>
2017-11-08 20:03:32 -0500

    Colas propias agregadas

---
+++ Assets/scriptsJuego/testCola.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testCola : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cola<int> cola = new Cola<int> ();
+		Debug.Log ("Cuenta?" + cola.Count());
+		cola.Enqueue (1);
+		cola.Enqueue (2);
+		cola.Enqueue (3);
+		Debug.Log ("Cuenta?" + cola.Count());
+		Debug.Log (cola.Peek ());
+		Debug.Log ("Cuenta?" + cola.Count());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
